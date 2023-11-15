eec9fe817ba0aa641cd098ec32b2afdc01475eab
baptistelechat <baptistelechat@outlook.fr>
2023-11-15 17:32:20 +0100

    💄 Change game display

--- app/src/components/GenerateImage.tsx
+++ app/src/components/GenerateImage.tsx
@@ -7,42 +7,51 @@ interface IGenerateImageProps {
 const GenerateImage = ({ currentGame }: IGenerateImageProps) => {
   return (
     <div className="w-1/3 h-full flex flex-col gap-4 justify-center">
-      <div className=" bg-secondary overflow-scroll">
-        <pre>
-          {currentGame ? JSON.stringify(currentGame, null, 4) : "Non défini"}
-        </pre>
-      </div>
-      <p className="italic">
-        {currentGame?.properties.Match.title[0]["plain_text"]}
-      </p>
       {currentGame ? (
-        <div className="flex gap-4 justify-center">
-          <div className="flex flex-col gap-2  items-center">
-            <Image
-              src={currentGame.properties["Domicile - Logo (sm)"].url}
-              width={150}
-              height={150}
-              alt="Home picture"
-            />
-            <p className="text-primary font-bold">
-              {currentGame.properties["Domicile - Final"].number}{" "}
-            </p>
-          </div>
-          <div className="flex flex-col gap-2  items-center">
-            <Image
-              src={currentGame.properties["Extérieur - Logo (sm)"].url}
-              width={150}
-              height={150}
-              alt="Away Picture"
-            />
-            <p className="text-primary font-bold">
-              {currentGame.properties["Extérieur - Final"].number}
-            </p>
+        <div className="flex flex-col gap-4 items-center">
+          <p className="italic">
+            {currentGame?.properties.Match.title[0]["plain_text"]}
+          </p>
+          <div className="flex gap-4 justify-center items-center">
+            <div className="flex flex-col gap-2 items-center">
+              <Image
+                src={currentGame.properties["Domicile - Logo (sm)"].url}
+                width={150}
+                height={150}
+                alt="Home picture"
+              />
+              <p className="text-primary font-bold">
+                {currentGame.properties["Domicile - Final"].number}{" "}
+              </p>
+            </div>
+            <div className="text-2xl text-primary font-bold">vs</div>
+            <div className="flex flex-col gap-2 items-center">
+              <Image
+                src={currentGame.properties["Extérieur - Logo (sm)"].url}
+                width={150}
+                height={150}
+                alt="Away Picture"
+              />
+              <p className="text-primary font-bold">
+                {currentGame.properties["Extérieur - Final"].number}
+              </p>
+            </div>
           </div>
         </div>
       ) : (
         <></>
       )}
+      <div
+        className={`h-1/2 p-4 bg-secondary overflow-scroll ${
+          currentGame
+            ? ""
+            : "text-xl text-primary font-bold flex justify-center items-center"
+        }`}
+      >
+        <pre>
+          {currentGame ? JSON.stringify(currentGame, null, 4) : "Non défini"}
+        </pre>
+      </div>
     </div>
   );
 };
